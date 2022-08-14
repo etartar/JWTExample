1acd2fa1cdc0c436d762a16e31c26f37951a76fd
Emir TARTAR <emirtartar35@gmail.com>
2022-08-14 18:19:58 +0300

    Add validate jwt token

---
+++ JWTExample/Attributes/AuthorizeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace JWTExample.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.Items["User"];
+            if (user is null)
+            {
+                context.Result = new JsonResult(new
+                {
+                    Message = "Unauthorized"
+                })
+                {
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+            }
+        }
+    }
+}
---
+++ JWTExample/Middlewares/JwtMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using JWTExample.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JWTExample.Middlewares
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public JwtMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context, TokenDbContext dbContext)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            if (token != null)
+            {
+                await AttachUserToContext(context, dbContext, token);
+            }
+
+            await _next(context);
+        }
+
+        private async Task AttachUserToContext(HttpContext context, TokenDbContext dbContext, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Token:SecurityKey"]));
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = key,
+                    ValidIssuer = _configuration["Token:Issuer"],
+                    ValidAudience = _configuration["Token:Audience"],
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = Guid.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+                context.Items["User"] = await dbContext.Users.FirstOrDefaultAsync(x => x.Id == userId);
+            }
+            catch (Exception)
+            {
+                //
+            }
+        }
+    }
+}
